a11a9479e9cade3657692de09a28672e270847cc
James Barret <jamesbarret715@gmail.com>
2024-04-26 23:07:16 +0200

    Implemented EntityFramework Core basics

--- Boolean/BotConfig.cs
+++ Boolean/BotConfig.cs
@@ -5,22 +5,26 @@ namespace Boolean;
 
 public class BotConfig
 {
-    public readonly string? Token;
+    public readonly string? Token, Database, Username, Password;
     public readonly Color BotTheme = Color.Gold;
     
     #if DEBUG
         public readonly ulong TestGuildId;
     #endif
     
-    private readonly IConfigurationRoot _config;
+    private readonly IConfigurationRoot _config = new ConfigurationBuilder()
+        // Forgive me father for I have sinned.
+        .SetBasePath(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName)
+        .AddJsonFile("settings.json")
+        .Build();
     
     public BotConfig()
     {
-        _config = new ConfigurationBuilder()
-            .AddUserSecrets<Program>()
-            .Build();
-
-        Token = _config["botToken"];
+        Token = _config["discordToken"];
+        Database = _config["dbName"];
+        Username = _config["dbUsername"];
+        Password = _config["dbPassword"];
+        
         
         #if DEBUG
             string? testGuildId = _config["testGuildId"];
--- Boolean/Program.cs
+++ Boolean/Program.cs
@@ -8,6 +8,8 @@ namespace Boolean;
 
 class Program
 {
+    private static DbContext _dbContext;
+    
     private static IServiceProvider _serviceProvider;
     private static DiscordSocketClient _client;
     private static BotConfig _config;
@@ -16,13 +18,13 @@ class Program
     static IServiceProvider CreateServices()
     {
         _client = new DiscordSocketClient();
-        _config = new BotConfig();
         _interactionService = new InteractionService(_client.Rest);
 
         var collection = new ServiceCollection()
             .AddSingleton(_interactionService)
             .AddSingleton(_client)
             .AddSingleton(_config)
+            .AddSingleton(_dbContext)
             .AddSingleton<DiscordSocketConfig>()
             .AddSingleton<EventHandlers>();
         
@@ -31,6 +33,14 @@ class Program
     
     public static async Task Main()
     {
+        // Load config
+        _config = new BotConfig();
+        
+        // Set up Postgres
+        _dbContext = new DbContext(_config);
+        await _dbContext.Database.EnsureCreatedAsync();
+        
+        // Set up Discord.net
         _serviceProvider = CreateServices();
         
         AttachEventHandlers();
