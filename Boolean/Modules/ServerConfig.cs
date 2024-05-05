cae8e00a5c4cf452be48805ff0782432d094556c
Kventis <kventis.gg@gmail.com>
2024-05-06 00:16:31 +0100

    Added /unset for removal of channel responsiblity

--- Boolean/Modules/ServerConfig.cs
+++ Boolean/Modules/ServerConfig.cs
@@ -42,21 +42,8 @@ public class ServerConfig
                 db.Servers.Add(server);
             }
 
-            string updatedChan = "";
-            switch (type) {
-                case ChannelTypes.Logs:
-                    updatedChan = "log";
-                    server.LogChannel = channel.Id;
-                    break;
-                case ChannelTypes.Starboard:
-                    updatedChan = "starboard";
-                    server.Starboard = channel.Id;
-                    break;
-                case ChannelTypes.Welcome:
-                    updatedChan = "welcome";
-                    server.Welcome = channel.Id;
-                    break;
-            }
+            string updatedChan = UpdateChannel(type, server, channel.Id);
+            
             await db.SaveChangesAsync();
             // might wanna make this sound less "robotic"
             embed.Description = $"{updatedChan} channel has been set to <#{channel.Id}>.";
@@ -102,5 +89,47 @@ public class ServerConfig
                     : $"The current {chan} channel is set to <#{chanid}>";
             await RespondAsync(embed: embed.Build(), ephemeral: true);
         }
+    }
+    // /unset, used to remove configuration options
+    [DefaultMemberPermissions(GuildPermission.Administrator)]
+    [Group("unset", "Unset configuration options")]
+    public class ServerUnset(DataContext db, Config config) 
+        : InteractionModuleBase<SocketInteractionContext>
+    {
+        // undoes channel configuration
+        [SlashCommand("channel", "Unmarks a channel for a certain purpose")]
+        public async Task ChannelUnset(ChannelTypes type)
+        {
+            var embed = new EmbedBuilder().WithColor(config.BotTheme);
+
+            Server? server = db.Servers.Find(Context.Guild.Id);
+            if (server == null) {
+                embed.Description = "This server does not have any configurations yet";
+                await RespondAsync(embed: embed.Build(), ephemeral: true);
+                return;
+            }
+            
+            string updatedChan = UpdateChannel(type, server, null);
+            
+            await db.SaveChangesAsync();
+            embed.Description = $"{updatedChan} channel has been unset.";
+            await RespondAsync(embed: embed.Build(), ephemeral: true);
+        }
+    }
+    public static string UpdateChannel(ChannelTypes type, Server server, ulong? value)
+    {
+        switch (type) {
+            case ChannelTypes.Logs:
+                server.LogChannel = value;
+                return "log";
+            case ChannelTypes.Starboard:
+                server.Starboard = value;
+                return "starboard";
+            case ChannelTypes.Welcome:
+                server.Welcome = value;
+                return "welcome";
+        }
+
+        return "";
     }
 }
